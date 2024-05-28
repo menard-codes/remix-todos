72456098cf6d31056b578955a3b4486d36cf50b1
Menard Maranan <menardmaranan.codes@gmail.com>
2024-05-29 07:57:08 +0800

    task item template

--- app/routes/todo.$id/route.tsx
+++ app/routes/todo.$id/route.tsx
@@ -1,5 +1,5 @@
 import { ActionFunctionArgs, json, LoaderFunctionArgs } from "@remix-run/node"
-import { useLoaderData, Form } from "@remix-run/react";
+import { useLoaderData, Form, useNavigate } from "@remix-run/react";
 import { DB } from "~/data/db";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
@@ -50,27 +50,43 @@ export const action = async ({ params, request }: ActionFunctionArgs) => {
 
 export default function TodoItem() {
     const { data } = useLoaderData<typeof loader>();
+    const navigate = useNavigate();
 
     return (
-        <Form method="PUT">
-            <div>
-                <label htmlFor="task">Task</label>
-                <input
-                    defaultValue={data.task}
-                    name="task"
-                    id="task"
-                    placeholder="Task item"
-                    required
-                />
+        <div className="min-h-screen grid place-content-center">
+            <div className="w-96">
+                <button onClick={() => navigate(-1)} type="button">
+                👈 Cancel
+                </button>
+                <h1 className="text-3xl font-semibold text-center p-4">Task Item</h1>
+                <Form method="PUT" className="w-full mx-auto grid gap-4">
+                    <div>
+                        <label htmlFor="task" className="block font-bold">Task</label>
+                        <input
+                            defaultValue={data.task}
+                            name="task"
+                            id="task"
+                            placeholder="Task item"
+                            className="bg-zinc-200 px-3 py-1 rounded min-w-full"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="completed" className="block font-bold">Status</label>
+                        <select
+                            name="completed"
+                            id="completed"
+                            defaultValue={data.completed ? "done" : "todo"}
+                            className="border-2 border-solid border-zinc-400 w-full p-2 rounded-lg"
+                            required
+                        >
+                            <option value="done">DONE</option>
+                            <option value="todo">TODO</option>
+                        </select>
+                    </div>
+                    <button className="px-4 py-2 mt-4 bg-zinc-500 text-zinc-50 rounded-xl">Save</button>
+                </Form>
             </div>
-            <div>
-                <label htmlFor="completed">Status</label>
-                <select name="completed" id="completed" defaultValue={data.completed ? "done" : "todo"} required>
-                    <option value="done">DONE</option>
-                    <option value="todo">TODO</option>
-                </select>
-            </div>
-            <button>Save</button>
-        </Form>
+        </div>
     )
 }
