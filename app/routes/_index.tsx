6e146fc846f5ae7ccea6ac4e1ad64dfb0ba062c7
Menard Maranan <menardmaranan.codes@gmail.com>
2024-05-28 20:59:54 +0800

    some UI modifications to action buttons

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -66,11 +66,11 @@ export default function Index() {
                     completed
                       ? <>
                           <input hidden name="completed" value="done" />
-                          <button className=" bg-green-600 text-green-50 px-4 py-1 rounded-md text-sm flex items-center">DONE</button>
+                          <button className=" bg-green-600 hover:bg-green-500 text-green-50 px-4 py-1 rounded-md text-sm flex items-center">DONE</button>
                         </>
                       : <>
                           <input hidden name="completed" value="todo" />
-                          <button className=" bg-zinc-600 text-zinc-50 px-4 py-1 rounded-md text-sm flex items-center">TODO</button>
+                          <button className=" bg-zinc-600 hover:bg-zinc-500 text-zinc-50 px-4 py-1 rounded-md text-sm flex items-center">TODO</button>
                         </>
                   }
                 </Form>
